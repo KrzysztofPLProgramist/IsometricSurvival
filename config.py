793dd74ed537883318bc52df22370c8ad34c5358
KrzysztofPLProgramist <krzysgraczowski@gmail.com>
2025-08-17 13:08:54 +0200

    0.0

---
+++ config.py
@@ -0,0 +1,5 @@
+import pygame
+import math
+import random
+
+pygame.init()
---
+++ main.py
@@ -0,0 +1,8 @@
+from draw import *
+
+class Game:
+    def __init__(self):
+        # Managers
+        self.tile_manager = TileManager()
+
+        #
